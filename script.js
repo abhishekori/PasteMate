f178c4bd44bf6127547da73515f209d405a900d2
abhishekori <abhikori1994@gmail.com>
2016-05-13 23:31:25 +0530

    just started

---
+++ script.js
@@ -0,0 +1,12 @@
+var app=angular.module('simpleSave',[]);
+
+app.controller('saveCtrl',function($scope) {
+var sub;
+var cont;
+  $scope.save=function(){
+sub=$scope.subject;
+cont=$scope.content;
+
+
+
+})
